fc71f16ee0c6e83bffda9d67257b2c96b4bb2d24
kutny <jirkakoutny@gmail.com>
2023-01-27 11:49:50 +0100

    missing route added

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 logger.info("sd2-depth-api started")
 
-from sd2_depth_api.routes import infer_images
+from sd2_depth_api.routes import infer_images, generate_depth_map
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4242)
